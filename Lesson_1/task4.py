f19e0d83668ed021bd306b5faf8cecd616449b16
EgorPodvalkov <egorpodvalkov@gmail.com>
2022-10-18 21:05:47 +0300

    l1t4 input fix

--- Lesson_1/task4.py
+++ Lesson_1/task4.py
@@ -1,12 +1,10 @@
 print ("--------------------------------------")
 
 print("Format ax^2+bx+c=0")
-#a=int(input("Enter a: "))
-#b=int(input("Enter b: "))
-#c=int(input("Enter c: "))
-a=1
-b=0
-c=0
+a=int(input("Enter a: "))
+b=int(input("Enter b: "))
+c=int(input("Enter c: "))
+
 disk=b**2-4*a*c
 if disk == 0:
     x=-b/(2*a)
@@ -18,8 +16,5 @@ elif disk>0:
     print("x2=",x2,sep="")
 else :
     print("There is no solution")
-
-
-
 
 print ("--------------------------------------")
