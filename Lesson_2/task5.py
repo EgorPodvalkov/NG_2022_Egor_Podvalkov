a06e95d079fea8f821b9328759ed0ae412ebed44
EgorPodvalkov <egorpodvalkov@gmail.com>
2022-10-26 16:51:50 +0300

    L2t5 reduced the number of 'for' to 1

--- Lesson_2/task5.py
+++ Lesson_2/task5.py
@@ -1,3 +1,7 @@
+from asyncio.windows_events import INFINITE
+from cmath import inf
+
+
 print("***********************************************")
 
 myStr=(input("Enter your list (use ', '): "))
@@ -5,17 +9,19 @@ lst=myStr.split(", ")
 
 max=int(lst[0])
 min=int(lst[0])
-sum=0
+sum=-2*int(lst[0])
+
 for item in lst:
     item=int(item)
-    if item>max:
+    if item>=max:
+        sum+=max
         max=item
-    if item<min:
+    elif item<=min:
+        sum+=min
         min=item
-for item in lst:
-    item=int(item)
-    if item!=max and item!=min:
+    else:
         sum+=item
+
 print(f"Max is {max}")        
 print(f"Min is {min}")        
 print(f"Sum is {sum}")        
