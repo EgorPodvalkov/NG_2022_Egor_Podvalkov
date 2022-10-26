fa546547e1992cef6eecc2999f986d7826ef838c
EgorPodvalkov <egorpodvalkov@gmail.com>
2022-10-26 18:26:32 +0300

    L2t4 remove some defects

--- Lesson_2/task4.py
+++ Lesson_2/task4.py
@@ -2,9 +2,11 @@ print("***********************************************")
 
 n=int(input("Enter natural number: "))
 
-i=2
-fact=1
-while i<=n:
-    fact=fact*i
+step=2
+factorial=1
+while step<=n:
+    factorial=factorial*step
+    step+=1
+print(factorial)
 
 print("***********************************************")
