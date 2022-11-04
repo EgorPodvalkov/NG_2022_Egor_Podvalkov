cba0a0f34c143138289079e9dbc6070ca7c7dbaf
EgorPodvalkov <egorpodvalkov@gmail.com>
2022-11-04 21:01:04 +0200

    L2t5 reduced the number of 'for' to 0

--- Lesson_2/task5.py
+++ Lesson_2/task5.py
@@ -5,11 +5,7 @@ lst=list(map(int,myStr.split(",")))
 
 max=max(lst)
 min=min(lst)
-sum=0
-
-for item in lst:
-    if item!=max and item!=min:
-        sum+=item
+sum=sum(lst)-min-max
 
 print(f"Max is {max}")        
 print(f"Min is {min}")        
