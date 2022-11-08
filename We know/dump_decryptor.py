2622f7d17351b7537deabd52bcb5623fe04e6d78
EgorPodvalkov <egorpodvalkov@gmail.com>
2022-11-08 12:14:15 +0200

    dump_decryptor full update

--- We know/dump_decryptor.py
+++ We know/dump_decryptor.py
@@ -1,29 +1,40 @@
-print("???????????????????????????????????????????????")
+from log_helper import *
 
-inputStep=int(input("Enter step(if 0, you will get all variants): "))
-cipher=input("Enter cipher:\n")
-# cipher="Terrgvatf, arjpbzre! Jr ner tynq gb frr, gung lbh unir fhpprffshyyl cnffrq bhe vavgvny grfg."
+def inputCipher():
+    return input("Enter cipher:\n")
 
-i=1
-while True:
-    if inputStep==0:
-        step=i
-    else:
-        step=inputStep
-    myStr=""
-    for item in cipher:
-        if item.upper()>"Z" or item.upper()<"A":
-            myStr+=item
+def inputStep():
+    try:
+        return int(input("\nEnter decrypt what you need: "))
+    except:
+        return 0
+
+def showAllDecrypts(cipher, maxLen=40):
+    for step in range(1,26):            # all possible steps 
+        decrypt(cipher, step, maxLen)
+
+def decrypt(cipher, step, maxLen=float("inf")):
+    print()
+    if step<=0 or step>=26:
+        info("Your cipher is: "+ cipher)
+        return
+    decryptedStr=str(step)+". "
+    for index in range(len(cipher)):     # brute force of string
+        if cipher[index].upper()>"Z" or cipher[index].upper()<"A":      # if symbol is not a letter
+            if index > maxLen:          # if cipher too long we show short version
+                decryptedStr+="..."
+                break
+            decryptedStr+=cipher[index]
             continue
-        if ord(item.upper())+step>ord("Z"):
-            item=chr(ord(item)+step-26)
-        else:
-            item=chr(ord(item)+step)
-        myStr+=item
-    print("\n"+str(step)+". "+myStr)
-    i+=1
-    if inputStep!=0 or i==26:
-        break
+        if ord(cipher[index].upper())+step>ord("Z"):            # decrypt rules
+            decryptedStr+=chr(ord(cipher[index])+step-26)
+        else:                                                   # decrypt rules
+            decryptedStr+=chr(ord(cipher[index])+step)
+    info(decryptedStr)       
 
+def main():
+    cipher=inputCipher()
+    showAllDecrypts(cipher)
+    decrypt(cipher, inputStep())
 
-print("???????????????????????????????????????????????")
+main()
